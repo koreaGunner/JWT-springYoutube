8b52d39f2a371d4025015a1a7b3530a9d25536ff
gunners <chakagae123@naver.com>
2024-07-07 01:49:25 +0900

    * 스프링 시큐리티 JWT 6 : 회원가입 로직 구현

---
+++ src/main/java/com/example/springjwt/service/JoinService.java
@@ -0,0 +1,38 @@
+package com.example.springjwt.service;
+
+import com.example.springjwt.dto.JoinDTO;
+import com.example.springjwt.entity.UserEntity;
+import com.example.springjwt.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JoinService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public void joinProcess(JoinDTO joinDTO) {
+
+        String username = joinDTO.getUsername();
+        String password = joinDTO.getPassword();
+
+        boolean isExist = userRepository.existsByUsername(username);
+
+        if (isExist) {
+            return;
+        }
+
+        UserEntity data = new UserEntity();
+
+        data.setUsername(username);
+        data.setPassword(bCryptPasswordEncoder.encode(password));
+        data.setRole("ROLE_ADMIN");
+
+        userRepository.save(data);
+    }
+}
